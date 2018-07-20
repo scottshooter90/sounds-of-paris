72007252be8e0a45630423cfdf51a01d7c6d7251
scottshooter90 <scottshooter90@gmail.com>
2018-07-21 00:28:39 +0100

    Added stylesheet

--- client.js
+++ client.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (
+const config = {
+	'title': 'Sounds of Paris'
+};
+
+class TitleBar extends React.Component {
+	style = {
+		height: '100px',
+		position: 'fixed',
+		backgroundColor: 'lightgrey'
+	};
+	render () {
+		return (
+			<header style={this.style}>
+				{config.title}
+			</header>
+		);
+	}
+};
+
+const app = (
 	<React.Fragment>
-		React is working
+		<TitleBar/>
 	</React.Fragment>
 );
 
-document.addEventListener ('DOMContentLoaded', ()=>ReactDOM.render (App, document.getElementById('app')));
+document.addEventListener (
+	'DOMContentLoaded',
+	() => {
+		document.title = config.title;
+		ReactDOM.render (app, document.getElementById('app'));
+	}
+);
