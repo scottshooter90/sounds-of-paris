e0387f3ada4e5273398e118bf77977bd7341c132
scottshooter90 <scottshooter90@gmail.com>
2018-07-21 09:49:05 +0100

    Added placeholder description and tweaked positioning

--- client.js
+++ client.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const config = {
-	'title': 'Sounds of Paris'
+	'titlePrefix': 'Sounds of',
+	'titleSuffix': 'Paris'
 };
 
 const bodyStyle = `
-	font-family: sans-serif
+	font-family: Lora,"Helvetica Neue",Helvetica,Arial,sans-serif;
 `;
 
 class BackgroundImage extends React.Component {
 	divStyle = {
-		backgroundImage: 'url("gare-bg.png")',
+		backgroundImage: 'url("'+this.props.src+'")',
 		height: '100%',
 		backgroundPosition: 'center',
 		backgroundRepeat: 'no-repeat',
@@ -29,43 +30,79 @@ class BackgroundImage extends React.Component {
 
 class TitleBar extends React.Component {
 	style = {
-		height: '50px',
+		//height: '50px',
 		position: 'absolute',
 		left: '0px',
 		top: '0px',
-		backgroundColor: 'lightgrey',
-		opacity: 0.5,
+		//backgroundColor: 'lightgrey',
+		//opacity: 0.5,
+		color: 'white',
 		width: '100%',
-		display: 'flex',
-		alignItems: 'center',
+		//display: 'flex',
+		//alignItems: 'center',
 		paddingLeft: '10px'
 	};
 	render () {
 		return (
 			<header style={this.style}>
-				{config.title}
+				<h2>
+					<span style={{fontWeight: 'normal'}}>
+						{config.titlePrefix.toUpperCase ()+' '}
+					</span>
+					{config.titleSuffix.toUpperCase ()}
+				</h2>
 			</header>
 		);
 	}
 };
+
+class DescriptionSection extends React.Component {
+	title = 'Gare Du Nord';
+	style = {
+		position: 'absolute',
+		top: '80px',
+		bottom: '80px',
+		//backgroundColor: 'lightgrey',
+		//opacity: 0.5,
+		color: 'white',
+		fontWeight: 'lighter',
+		padding: '10px',
+		marginLeft: '80px',
+		marginRight: '80px',
+		overflow: 'scroll'
+	}
+	render () {
+		return (
+			<div style={this.style}>
+				<h3>
+					{this.title}
+				</h3>
+				<p>
+In addition to being a national railway terminal, Gare du Nord is also a massive regional MTR interchange. The recording starts at the Eurostar-reserved platforms, goes on through to the national and regional departure areas, and then continues down to the lower levels where the underground express service operates. The last part follows the interchange tunnel linking to the La Chapelle station on Metro line 2. The total distance, on foot, is close to 800m and is spent entirely underground.
+				</p>
+			</div>
+		)
+	}
+}
 
 class MediaBar extends React.Component {
 	style = {
 		position: 'absolute',
 		bottom: '0px',
 		left: '0px',
-		height: '50px',
+		//height: '80px',
 		width: '100%',
-		opacity: 0.5,
+		//opacity: 0.5,
 		display: 'flex',
 		alignItems: 'center',
 		paddingLeft: '10px',
-		backgroundColor: 'lightgrey'
+		//backgroundColor: 'lightgrey',
+		color: 'white'
 	};
 	render () {
 		return (
 			<footer style={this.style}>
-				PLAY
+				<h2>PLAY</h2>
 			</footer>
 		);
 	}
@@ -73,9 +110,11 @@ class MediaBar extends React.Component {
 
 const app = (
 	<React.Fragment>
-		<BackgroundImage/>
+		<BackgroundImage
+			src='./intro-bg.jpg'
+		/>
 		<TitleBar/>
-
+		<DescriptionSection/>
 		<MediaBar/>
 	</React.Fragment>
 );
@@ -83,7 +122,7 @@ const app = (
 document.addEventListener (
 	'DOMContentLoaded',
 	() => {
-		document.title = config.title;
+		document.title = config.titlePrefix + ' ' +config.titleSuffix;
 		document.body.style = bodyStyle;
 		ReactDOM.render (app, document.getElementById('app'));
 	}
